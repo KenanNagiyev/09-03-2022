0b6d9617d1aa9e04407ebb570a47f5e57f166fda
Kanan Naghiyev <tu788lnmp@aztu.local>
2022-03-09 14:27:19 +0400

    new file

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,13 +8,13 @@ namespace ConsoleApp1
         {
             Console.WriteLine(HasLetter("salam eleykum","salam"));
         }
-        static bool HasLetter(string name, string letter)
+        static bool HasLetter(string name, string word)
         {
             for (int i = 0; i < name.Length; i++)
-                for (int j = 0; j < letter.Length; j++)
+                for (int j = 0; j < word.Length; j++)
             
             {
-                if (name[i] == letter[i])
+                if (name[i] == word[i])
                 {
                     return true;
                 }
